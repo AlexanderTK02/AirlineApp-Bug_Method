a8e9264fa7576b5369cf9b195e2034d67ec5e307
AlexanderTK <abutahmile02@hotmail.com>
2026-04-22 02:18:46 +0200

    Bug Fix: Changed out "/" to a "*" to multiply instead of dividing. Line11

--- AirlineApp/Airline.Infrastructure/Factories/SeatPriceFactory.cs
+++ AirlineApp/Airline.Infrastructure/Factories/SeatPriceFactory.cs
@@ -5,9 +5,9 @@ namespace Airline.Infrastructure.Factories;
 
 public class SeatPriceFactory : ISeatPriceFactory
 {
-    // BUG_TARGET: CalculatePrice
+    // BUG_TARGET: CalculatePrice |||*FIXED*|||
     public decimal CalculatePrice(decimal basePrice, SeatClass seatClass)
     {
-        return Math.Round(basePrice / seatClass.GetPriceMultiplier(), 2);
+        return Math.Round(basePrice * seatClass.GetPriceMultiplier(), 2); // <== Bug was right here //
     }
 }
